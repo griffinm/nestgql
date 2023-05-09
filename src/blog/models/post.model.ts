e2bcaa079024f8a5e1731978df9af1d0a900c7e8
Griffin <griffin@yardzen.com>
2023-05-09 18:59:45 -0400

    Checkpoint before validation

--- src/blog/models/post.model.ts
+++ src/blog/models/post.model.ts
@@ -1,20 +1,21 @@
 import { Field, ObjectType, ID } from '@nestjs/graphql';
+import { Post as PostDB } from '@prisma/client';
 import { Author } from './author.model';
 
 @ObjectType({ description: 'Post model' })
 export class Post {
   @Field(type => ID)
-  id: number;
+  id: PostDB['id'];
 
-  @Field()
-  title: string;
+  @Field(type => String)
+  title: PostDB['title'];
 
-  @Field()
-  content: string;
+  @Field(type => String)
+  content: PostDB['content'];
 
   @Field(type => Date)
-  createdAt: Date;
+  createdAt: PostDB['createdAt'];
 
-  @Field()
-  authorId: number;
+  @Field(type => Number)
+  authorId: PostDB['authorId'];
 }
--- src/blog/resolvers/post.resolver.ts
+++ src/blog/resolvers/post.resolver.ts
@@ -1,13 +1,31 @@
-import { Resolver, Query, Args, ResolveField, Parent } from "@nestjs/graphql";
+import { Resolver, Query, Args, ResolveField, Parent, Mutation } from "@nestjs/graphql";
 import { Post } from "../models/post.model";
-import { PostService } from "../services/post.service";
-import { Author } from "../models/author.model";
-import { AuthorService } from "../services/author.service";
+import { PrismaService } from "../services/prisma.service";
 
-@Resolver(of => Post)
+@Resolver(() => Post)
 export class PostResolver {
   constructor(
-    private readonly postService: PostService,
-    private readonly authorService: AuthorService,
+    private readonly prisma: PrismaService,
   ) {}
+
+  @Query(() => [Post])
+  async posts(
+    @Args(({ name: 'title', type: () => String, nullable: true })) title: string,
+  ): Promise<Post[]> {
+    return this.prisma.post.findMany({ where: { title } });
+  }
+
+  @Query(() => Post)
+  async post(@Args('id') id: number): Promise<Post> {
+    return this.prisma.post.findUnique({ where: { id } });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Args(({ name: 'title', type: () => String })) title: string,
+    @Args(({ name: 'content', type: () => String, nullable: true })) content: string,
+    @Args(({ name: 'authorId', type: () => Number })) authorId: number,
+  ) : Promise<Post> {
+    return this.prisma.post.create({ data: { title, content, authorId } });
+  }
 }
